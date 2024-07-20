c7aadd02cac92abd01ec1c05cbce7c94db57b045
Creative_Dreamer <97633322+DevJohannes@users.noreply.github.com>
2024-07-21 00:47:39 +0200

    Update database.py
    removed class

--- src/user/database.py
+++ src/user/database.py
@@ -1,7 +1,21 @@
 import sqlite3
 
+PATH = 'user.db'
 
-class DatabaseManager:
-    def __init__(self):
-        self.path = 'user.db'
 
+# to set the database up
+def create_db(self):
+    conn = sqlite3.connect(self.path)
+    cursor = conn.cursor()
+
+    # Creating table
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS users (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            username TEXT NOT NULL UNIQUE,
+            password TEXT NOT NULL
+            )
+    ''')
+
+    conn.commit()
+    conn.close()
