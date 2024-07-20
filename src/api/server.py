c1b936f55e22e6d8761e157c7836b4fd53212e80
Creative_Dreamer <97633322+DevJohannes@users.noreply.github.com>
2024-07-20 23:41:20 +0200

    Update server.py
    server create route

--- src/api/server.py
+++ src/api/server.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from src.ServerManager import servers
 
-app = Flask(__name__) # Creating flask instance
+app = Flask(__name__)  # Creating flask instance
 
 
 # Home Directory
@@ -10,13 +10,19 @@ def home():
     return 'Flask is running'
 
 
-# List of all server types
+# Route for the list of all server types
 @app.route('/api/server/types')
 def server_types():
     return str(servers.manager.list_server_names())
 
 
+# Route for creating a new server
+@app.route('/api/server/create')
+def create_server():
+    return 'test'
+
+
 # Start function
 def start(port: int, host: str, debug: bool):
-    print('Starting api on ' + host + ':' + str(port) + ' with debugging ' + str(debug)) # logging
-    app.run(port=port, debug=debug) # running app
+    print('Starting api on ' + host + ':' + str(port) + ' with debugging ' + str(debug))  # logging
+    app.run(port=port, debug=debug)  # running app
