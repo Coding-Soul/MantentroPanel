ff1b410f957cdf18e033b18f813ef08b6f0b9937
Creative_Dreamer <97633322+DevJohannes@users.noreply.github.com>
2024-07-20 23:07:28 +0200

    Update server_manager.py

--- src/ServerManager/server_manager.py
+++ src/ServerManager/server_manager.py
@@ -1,13 +1,17 @@
-from server_type import ServerType
+from src.ServerManager.server_type import ServerType
 
 
 class ServerManager:
-    def __int__(self):
+    def __init__(self):
         self.servers = []
 
-    def add_server(self, server_type: ServerType):
-        server = server_type
+    def add_server(self, name: str, configuration:dict):
+        server = ServerType(name=name, configuration=configuration)
         self.servers.append(server)
+
+    def list_server_names(self):
+        for server in self.servers:
+            return server.name
 
     def get_config(self):
         pass
