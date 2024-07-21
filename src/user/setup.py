4ab562e3a48bee64b290ca9d1e6ac07a59552f14
Creative_Dreamer <97633322+DevJohannes@users.noreply.github.com>
2024-07-21 14:25:45 +0200

    Update setup.py

--- src/user/setup.py
+++ src/user/setup.py
@@ -14,6 +14,7 @@ def create_db():
         CREATE TABLE IF NOT EXISTS users (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
             username TEXT NOT NULL UNIQUE,
+            email TEXT NOT NULL UNIQUE,
             password TEXT NOT NULL
             )
     ''')
