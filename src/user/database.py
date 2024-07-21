8e80bab586402b0f2bce6e5ca8dfa1c797a2788b
Creative_Dreamer <97633322+DevJohannes@users.noreply.github.com>
2024-07-21 14:25:39 +0200

    Update database.py

--- src/user/database.py
+++ src/user/database.py
@@ -5,3 +5,19 @@ PATH = 'users.db'
 
 def create_user(username, email, password):
     conn = sqlite3.connect(PATH)
+    cursor = conn.cursor()
+
+    try:
+        cursor.execute('''
+            INSERT INTO users (username, email, password)
+            VALUES (?,?,?)
+        
+        ''', (username, email, password))
+        conn.commit()
+        print(username + " was successfully created!")
+        return 0
+    except sqlite3.IntegrityError:
+        print('Error: Username must be unique')
+        return 1
+    finally:
+        conn.close()
