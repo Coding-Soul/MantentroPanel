59a1b49cb8161c49760bfcf49fd4034355a85361
Creative_Dreamer <97633322+DevJohannes@users.noreply.github.com>
2024-07-22 20:50:59 +0200

    Update server.py

--- src/api/server.py
+++ src/api/server.py
@@ -1,18 +1,21 @@
 from flask import Flask
+from src.config.Reader import read_config
 
 # Route imports
 from src.api.routes.home import home_bp
 from src.api.routes.servers import servers_bp
 from src.api.routes.users import users_bp
-
+from src.api.routes.login import login_bp
 
 app = Flask(__name__)  # Creating flask instance
-
 
 # Routes register
 app.register_blueprint(home_bp)
 app.register_blueprint(servers_bp)
 app.register_blueprint(users_bp)
+app.register_blueprint(login_bp)
+
+app.secret_key = read_config('key')
 
 
 # Start function
