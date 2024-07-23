2634d63fc6ca752e153f7aeaeb5ff3bda825f06d
Creative_Dreamer <97633322+DevJohannes@users.noreply.github.com>
2024-07-23 17:55:21 +0200

    Update servers.py

--- src/ServerManager/servers.py
+++ src/ServerManager/servers.py
@@ -3,6 +3,7 @@ from src.ServerManager.server_type import ServerType
 
 manager = ServerManager()
 
-manager.add_server_type('Pycord', {})
+# Installations
+PYCORD = manager.add_server_type('Pycord', {})
 
 print(manager.list_server_names())
