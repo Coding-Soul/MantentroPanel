0f7b8c9e2273edba698325f05b891f2e63170a24
Creative_Dreamer <97633322+DevJohannes@users.noreply.github.com>
2024-07-24 22:06:50 +0200

    Update Reader.py
    fixed issue

--- src/config/Reader.py
+++ src/config/Reader.py
@@ -1,6 +1,18 @@
 import json
+import os
 
 
 def read_config(arg):
-    with open('Z:\Coding\Projekte\Coding Soul\MantentroPanel\src\config\secret_key.json') as f:
-        return json.load(f)[arg]
+    file_path = r'Z:\Coding\Projekte\Coding Soul\MantentroPanel\src\config\secret_key.json'
+
+    try:
+        with open(file_path, 'r') as f:
+            return json.load(f)[arg]
+    except FileNotFoundError:
+        dictionary = {
+            "key": f"{os.urandom(24)}"
+        }
+        with open(file_path, 'w') as f:
+            json.dump(dictionary, f, ensure_ascii=False, indent=4)
+
+        return dictionary[arg]
